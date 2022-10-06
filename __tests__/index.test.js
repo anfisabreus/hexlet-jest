e1cabb714f4cf8a334213dd09b6e3e5bd07c4d0e
anfisabreus <anfisabreus@gmail.com>
2022-10-06 10:24:54 +0300

    jest tests folder

---
+++ __tests__/index.test.js
@@ -0,0 +1,7 @@
+// запуск теста NODE_OPTIONS=--experimental-vm-modules npx jest
+import reverse from '../src/index.js';
+
+test('reverse', () => {
+  expect(reverse('hello')).toEqual('olleh');
+  expect(reverse('')).toEqual('');
+});
